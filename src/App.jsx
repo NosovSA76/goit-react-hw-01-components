d289959753d252226e71a62aabfdb4ed4e4a6633
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-07-03 14:00:12 +0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import user from './Profile/user.json';
+import { Profile } from './Profile/Profile';
+
+console.log(user)
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+
+    </div>
+  );
+};
